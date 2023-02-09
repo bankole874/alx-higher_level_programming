c50d99817d636c71142391c3804a12b06ece47c5
hbankole <damilarehammed.hd@gmail.com>
2023-02-09 06:40:40 -0800

    added pass to last line

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-    """ Create class square. """
+
+"""Create class square"""
+
 class Square:
-    """ Class Square that defines a square.  """
+    """Class Square that defines a square"""
+
     pass
+
