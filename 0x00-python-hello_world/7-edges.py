1e290bd700ac63d0bcb6c4c4026fbe0a7b1905b5
Hammed Bankole <40712924+bankole874@users.noreply.github.com>
2023-11-27 20:03:33 +0000

    added7 -edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -6,7 +6,3 @@ middle_word = word[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
-
-# First 3 letters: Hol
-# Last 2 letters: on
-# Middle word: olberto
