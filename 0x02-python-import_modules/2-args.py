d1d6c7790617cd0b8b21b100d7498d7c53386168
Hammed Bankole <40712924+bankole874@users.noreply.github.com>
2023-12-01 20:02:41 +0000

    updated 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-if len(sys.argv) > 1:
-    # for i in range(len(sys.argv)-1):
-    #     if (i > 0):
-    #         print(f"{sys.argv[i]} ", end='')
-    if (len(sys.argv) == 1):
-        print(f"{len(sys.argv)-1} argument:")
+    if len(sys.argv) > 1:
+        # for i in range(len(sys.argv)-1):
+        #     if (i > 0):
+        #         print(f"{sys.argv[i]} ", end='')
+        if (len(sys.argv) == 1):
+            print(f"{len(sys.argv)-1} argument:")
+        else:
+            print(f"{len(sys.argv)-1} arguments:")
+        for i in range(len(sys.argv)-1):
+            print(f"{i+1}: {sys.argv[i+1]}")
     else:
-        print(f"{len(sys.argv)-1} arguments:")
-    for i in range(len(sys.argv)-1):
-        print(f"{i+1}: {sys.argv[i+1]}")
-else:
-    print("0 arguments.")
+        print("0 arguments.")
