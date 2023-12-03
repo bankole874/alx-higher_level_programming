15dcf225f0779ee963b4438803fb45d829241778
Hammed Bankole <40712924+bankole874@users.noreply.github.com>
2023-12-03 04:21:13 +0000

    updated README.md

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+result = 0
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        for arg in len(sys.argv):
+            if arg[2]
+        
+
+# guillaume@ubuntu:~/0x02$ ./100-my_calculator.py ; echo $?
+# Usage: ./100-my_calculator.py <a> <operator> <b>
+# 1
+# guillaume@ubuntu:~/0x02$ ./100-my_calculator.py 3 + 5 ; echo $?
+# 3 + 5 = 8
+# 0
+# guillaume@ubuntu:~/0x02$ ./100-my_calculator.py 3 H 5 ; echo $?
+# Unknown operator. Available operators: +, -, * and /
+# 1
