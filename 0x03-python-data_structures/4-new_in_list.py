cfcb7a2260b5a56aa59d55082e7d32998e7f54bd
bankole874 <damilarehammed.hd.com>
2023-12-04 21:08:20 +0100

    updated 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    list = [];
+    list = []
     if idx > 0 and idx < (len(my_list) - 1):
         for i, e in enumerate(my_list):
             if i != idx:
