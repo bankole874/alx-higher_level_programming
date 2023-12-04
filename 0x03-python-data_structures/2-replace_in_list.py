a7b7e0861b5d6084ab435f4fb4b31dedf6f5fc3b
bankole874 <damilarehammed.hd.com>
2023-12-04 16:00:37 +0100

    updated 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -8,7 +8,3 @@ def replace_in_list(my_list, idx, element):
             list.append(element)
     my_list[idx] = element
     return (list)
-
-# guillaume@ubuntu:~/0x03$ ./2-main.py
-# [1, 2, 3, 9, 5]
-# [1, 2, 3, 9, 5]
