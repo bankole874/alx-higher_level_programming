cd79db0d4ac40feba73ea9604e63c690b1962d8c
bankole874 <damilarehammed.hd.com>
2023-12-04 16:05:36 +0100

    updated 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,9 +2,6 @@
 def replace_in_list(my_list, idx, element):
     list = []
     for i, e in enumerate(my_list):
-        if i != idx:
-            list.append(e)
-        else:
-            list.append(element)
-    my_list[idx] = element
-    return (list)
+        if i == idx:
+            my_list[idx] = element
+    return (my_list)
