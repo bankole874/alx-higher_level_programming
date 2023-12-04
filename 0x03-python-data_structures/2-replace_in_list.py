4d1233ea08e35e8692897aef81c85f08879c5f04
bankole874 <damilarehammed.hd.com>
2023-12-04 15:58:32 +0100

    updated 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    
+    list = []
+    for i, e in enumerate(my_list):
+        if i != idx:
+            list.append(e)
+        else:
+            list.append(element)
+    my_list[idx] = element
+    return (list)
+
+# guillaume@ubuntu:~/0x03$ ./2-main.py
+# [1, 2, 3, 9, 5]
+# [1, 2, 3, 9, 5]
