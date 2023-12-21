ea1bfc287c1d09529e1a240d895059ae6f7d3fa9
bankole874 <damilarehammed.hd.com>
2023-12-21 18:18:31 +0100

    updated 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         division = a / b
-    except:
+    except Exception:
         division = None
     finally:
         print("Inside result: {}".format(division))
